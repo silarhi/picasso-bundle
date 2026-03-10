4162e164b4b9f90524951579ca7b76eccb1bd962
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-03-10 13:45:55 +0100

    create and store fixture images

--- tests/Service/MetadataGuesserTest.php
+++ tests/Service/MetadataGuesserTest.php
@@ -30,7 +30,8 @@ class MetadataGuesserTest extends TestCase
 
     public function testGuessGifImage(): void
     {
-        $gif = base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+        $gif = file_get_contents(dirname(__DIR__) . '/Fixtures/pixel.gif');
+        self::assertNotFalse($gif);
         $stream = $this->createStream($gif);
 
         $result = $this->guesser->guess($stream);
@@ -42,7 +43,8 @@ class MetadataGuesserTest extends TestCase
 
     public function testGuessPngImage(): void
     {
-        $png = base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAIAAAADCAYAAACo2gZnAAAADklEQVQI12P4z8BQDwAEgAF/QualzQAAAABJRU5ErkJggg==');
+        $png = file_get_contents(dirname(__DIR__) . '/Fixtures/2x3.png');
+        self::assertNotFalse($png);
         $stream = $this->createStream($png);
 
         $result = $this->guesser->guess($stream);
@@ -89,7 +91,8 @@ class MetadataGuesserTest extends TestCase
 
     public function testGuessReadsFromOffset(): void
     {
-        $gif = base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+        $gif = file_get_contents(dirname(__DIR__) . '/Fixtures/pixel.gif');
+        self::assertNotFalse($gif);
         $stream = $this->createStream($gif);
 
         // Advance the stream position
@@ -107,7 +110,8 @@ class MetadataGuesserTest extends TestCase
         $cache = new ArrayAdapter();
         $guesser = new MetadataGuesser($cache);
 
-        $gif = base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+        $gif = file_get_contents(dirname(__DIR__) . '/Fixtures/pixel.gif');
+        self::assertNotFalse($gif);
         $stream = $this->createStream($gif);
 
         // First call: populates cache
@@ -129,7 +133,8 @@ class MetadataGuesserTest extends TestCase
         $cache = new ArrayAdapter();
         $guesser = new MetadataGuesser($cache);
 
-        $gif = base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+        $gif = file_get_contents(dirname(__DIR__) . '/Fixtures/pixel.gif');
+        self::assertNotFalse($gif);
         $stream = $this->createStream($gif);
 
         $result = $guesser->guess($stream);
